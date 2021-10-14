8acf7898361327338762a10b510d3f8686dcb211
Artur Babayan <Artur.Babayan@acba.am>
2021-10-14 18:06:15 +0400

    first commit

---
+++ sripts/index.js
@@ -0,0 +1,58 @@
+var configJson = {
+    row: 4,
+    column: 10,
+    questions: [
+        'Դուք շահում եք "Bounty"',
+        'Դուք շահում եք "Snickers"',
+        'Դուք շահում եք "Twix"',
+        'Հակոբի ավագ որդուն անունը',
+        'Ո՞վ էր Կիսը'
+    ]
+}
+var draw = function () {
+    configJson.questions = shuffle(configJson.questions);
+    let wrapper = document.getElementsByClassName("wrapper")[0].classList.remove("hide");
+    let board = document.getElementById("board");
+    for (let i = 0; i < configJson.row; i++) {
+        let div = document.createElement("div");
+        div.classList.add("row");
+        for (let j = 0; j < configJson.column; j++) {
+            div.innerHTML += "<button class='button' id='i_" + (i*configJson.column+j) + "' onclick='getQuastion(" + (i*configJson.column+j) + ", this)'>" + (i*configJson.column+j+1) + "</button>";
+        }
+        board.append(div);        
+    }
+    let newGameButton = document.getElementById("newGame");
+    newGameButton.classList.add("hide");
+}
+var getQuastion = function (i, el) {
+    let overlay = document.getElementsByClassName("overlay")[0];
+    overlay.classList.replace("jq-hide", "jq-show");
+    console.log(el);
+    el.classList.add("clicked");
+    el.setAttribute("disabled",true);
+    let question = document.getElementById("question");
+    question.innerText = configJson.questions[i];
+}
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+function closeDialog() {
+    let overlay = document.getElementsByClassName("overlay")[0];
+    overlay.classList.replace("jq-show", "jq-hide");
+}
